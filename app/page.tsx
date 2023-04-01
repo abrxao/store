d259b34c185cf1459fbb28598a3bf6f275212969
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-04-01 18:25:06 -0300

    feat: init pagination of search results

--- app/page.tsx
+++ app/page.tsx
@@ -5,29 +5,72 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [data, setData] = useState({ poster: "" });
+  const [search, setSearch] = useState<string>("");
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState<number>(0);
 
-  async function teste() {
-    const { data } = await axios.get("api/top20");
+  const handleSearch = async () => {
+    const { data } = await axios({
+      method: "post",
+      url: "api/search",
+      data: {
+        search: search,
+        page: page,
+      },
+    });
     setData(data);
-  }
+  };
 
-  useEffect(() => {
-    teste();
-  }, []);
+  const handlePrev = async () => {
+    setPage((page) => page - 1);
+    handleSearch();
+  };
+
+  const handleNext = () => {
+    setPage((page) => page + 1);
+    handleSearch();
+  };
 
   return (
-    <>
-      <h1>teste</h1>
-      {data.poster != "" && (
+    <div>
+      <div className="p-2 w-full flex flex-col items-center justify-center">
+        <label htmlFor="genres_input">procure por genero</label>
+        <input
+          type="text"
+          id="genres_input"
+          className="border p-1 bg-black text-white rounded-md"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className="p-2 border bg-gray-200" onClick={handleSearch}>
+          search
+        </button>
+        <div className="flex gap-1 my-2">
+          <button
+            className="p-1 bg-gray-200"
+            onClick={handlePrev}
+            disabled={page === 1}
+          >
+            prev
+          </button>
+          <button
+            className="p-1 bg-gray-200"
+            disabled={data.length < 20}
+            onClick={handleNext}
+          >
+            next
+          </button>
+        </div>
+        <h2>page {page + 1}</h2>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+      {/* {data.poster != "" && (
         <Image
           src={data.poster as string}
           width={200}
           height={200}
           alt="movie poster"
         />
-      )}
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </>
+      )} */}
+    </div>
   );
 }
